58fcb2c6a7979f95ee6f8b6a9c81c64a2a599527
kumudinidesale <kdesale1211@gmail.com>
2022-02-23 22:21:51 +0530

    added:Add Contact In AddressBook

--- src/com/java/AddressBook.java
+++ src/com/java/AddressBook.java
@@ -1,12 +1,41 @@
 package com.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
-	public static void main(String[] args) {
-		int year = 2021;
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-			System.out.println("Leap Year");
-		} else {
-			System.out.println("Common Year");
-		}
-	}
+	static Scanner scanner = new Scanner(System.in);
+	ArrayList<Contact> personInformation = new ArrayList<Contact>();
+
+	// add new person record to array list after taking input
+
+	public Contact addPersonContact() {
+		System.out.println("Enter the First Name");
+		String fName = scanner.next();
+		System.out.println("Enter the Last Name");
+		String lName = scanner.next();
+		System.out.println("Enter the Address");
+		String address = scanner.next();
+		System.out.println("Enter the City");
+		String city = scanner.next();
+		System.out.println("Enter the State");
+		String state = scanner.next();
+		System.out.println("Enter the Zip");
+		String zip = scanner.next();
+		System.out.println("Enter the PhoneNumber");
+		String phoneNumber = scanner.next();
+		System.out.println("Enter the Email");
+		String email = scanner.next();
+		// creating constructor
+		Contact contacts = new Contact(fName, lName, address, city, state, zip, phoneNumber, email);
+
+		// add the above list to to Contacts array list
+		personInformation.add(contacts);
+
+		// printing contacts object data inside
+		System.out.println(contacts);
+
+		return contacts;
+
+	}// end of add person method
 }
